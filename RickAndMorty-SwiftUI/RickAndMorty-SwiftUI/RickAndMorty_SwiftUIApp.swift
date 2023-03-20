424bc04fc903cd6a016700ac0b7b81ba9dee58e3
Mert Urhan <mert-urhan@hotmail.com>
2023-03-20 12:13:32 +0300

    Project initialized
    New project created

---
+++ RickAndMorty-SwiftUI/RickAndMorty-SwiftUI/RickAndMorty_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  RickAndMorty_SwiftUIApp.swift
+//  RickAndMorty-SwiftUI
+//
+//  Created by Mert Urhan on 20.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct RickAndMorty_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
